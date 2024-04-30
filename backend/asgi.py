e2055aea41c9f9c05927f8b11b989248b768c42f
Smark16 <sengendomark16@gmail.com>
2024-04-30 12:26:24 +0300

    modified asgi

--- backend/asgi.py
+++ backend/asgi.py
@@ -6,23 +6,29 @@ It exposes the ASGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
 """
-
 import os
-
 from django.core.asgi import get_asgi_application
-from channels.routing import ProtocolTypeRouter, URLRouter
-from channels.auth import AuthMiddlewareStack
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
+
+# Initialize Django ASGI application early to ensure the app is loaded
+# before importing the channels routing
+django_asgi_app = get_asgi_application()
+
+# Now we can import the rest of the channels packages and the routing
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
 import restaurant.routing
 
+# Define the application protocol type router
 application = ProtocolTypeRouter({
-    'http':get_asgi_application(),
-    'websocket':AuthMiddlewareStack(
+    'http': django_asgi_app,  # Use the ASGI application we initialized earlier
+    'websocket': AuthMiddlewareStack(
         URLRouter(
-          restaurant.routing.websocket_urlpatterns
+            restaurant.routing.websocket_urlpatterns
         )
-    )
+    ),
 })
+
 
 app = application
