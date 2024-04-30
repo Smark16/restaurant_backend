ed3381d06f097d81602a9b18449593c65d08a6b7
Smark16 <sengendomark16@gmail.com>
2024-04-30 11:13:56 +0300

    added redis

--- backend/settings.py
+++ backend/settings.py
@@ -72,12 +72,13 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 ASGI_APPLICATION = 'backend.asgi.application'
 
 CHANNEL_LAYERS = {
-       'default':{
-           'BACKEND':'channels.layers.InMemoryChannelLayer'
-       }
+    'default': {
+        'BACKEND': 'channels_redis.core.RedisChannelLayer',
+        'CONFIG': {
+            "hosts": [('127.0.0.1', 6379)],  # Set this to your Redis server's address
+        },
+    },
 }
-
-DEFAULT_CHANNEL_LAYER = 'default'
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
