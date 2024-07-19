3974dff36f8acd5ab624402327c8fbcc4eaf7127
Smark16 <sengendomark16@gmail.com>
2024-07-19 16:54:14 +0300

    merged

--- backend/asgi.py
+++ backend/asgi.py
@@ -1,13 +1,16 @@
 import os
 from django.core.asgi import get_asgi_application
-from channels.auth import AuthMiddlewareStack
-from channels.routing import ProtocolTypeRouter, URLRouter
-import restaurant.routing
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
 
-# Initialize the ASGI application
+# Initialize Django ASGI application early to ensure the app is loaded
+# before importing the channels routing
 django_asgi_app = get_asgi_application()
+
+# Now we can import the rest of the channels packages and the routing
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+import restaurant.routing
 
 # Define the application protocol type router
 application = ProtocolTypeRouter({
