fda3dbd564f74e2d5039f683310c78ff07d069a8
Smark16 <sengendomark16@gmail.com>
2024-07-19 16:35:09 +0300

    merged

--- backend/asgi.py
+++ backend/asgi.py
@@ -7,11 +7,11 @@ import restaurant.routing
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
 
 # Initialize the ASGI application
-application = get_asgi_application()
+django_asgi_app = get_asgi_application()
 
 # Define the application protocol type router
 application = ProtocolTypeRouter({
-    'http': application,  # Use the ASGI application we initialized earlier
+    'http': django_asgi_app,  # Use the ASGI application we initialized earlier
     'websocket': AuthMiddlewareStack(
         URLRouter(
             restaurant.routing.websocket_urlpatterns
