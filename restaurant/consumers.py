a974dbe59870bdc311f9874acc20b617e5d644e9
Smark16 <sengendomark16@gmail.com>
2024-07-24 17:09:17 +0300

    merge

--- restaurant/consumers.py
+++ restaurant/consumers.py
@@ -5,9 +5,12 @@ from channels.db import database_sync_to_async
 
 class ChatConsumer(AsyncWebsocketConsumer):
     async def connect(self):
+        # Retrieve the user ID from the URL route
         self.user_id = self.scope['url_route']['kwargs']['user_id']
+        # Define room group based on user ID
         self.room_group_name = f'user_{self.user_id}'
-        
+
+        # Join the room group
         await self.channel_layer.group_add(
             self.room_group_name,
             self.channel_name
@@ -15,6 +18,7 @@ class ChatConsumer(AsyncWebsocketConsumer):
         await self.accept()
 
     async def disconnect(self, close_code):
+        # Leave the room group
         await self.channel_layer.group_discard(
             self.room_group_name,
             self.channel_name
@@ -22,24 +26,28 @@ class ChatConsumer(AsyncWebsocketConsumer):
 
     async def receive(self, text_data):
         text_data_json = json.loads(text_data)
-        message = text_data_json['message']
-        user_id = text_data_json['user']
+        message = text_data_json.get('message')
+        user_id = text_data_json.get('user')
 
+        print(message, user_id)
+        # Save the message to the database
         await self.save_message(message, user_id)
 
+        # Broadcast the message to the room group
         await self.channel_layer.group_send(
             self.room_group_name,
             {
-                'type': 'notification',
+                'type': 'notification_message',
                 'message': message,
                 'user': user_id
             }
         )
 
-    async def notification(self, event):
+    async def notification_message(self, event):
         message = event['message']
         user_id = event['user']
 
+        # Send the message to WebSocket
         await self.send(text_data=json.dumps({
             'type': 'notification',
             'message': message,
@@ -47,6 +55,10 @@ class ChatConsumer(AsyncWebsocketConsumer):
         }))
 
     @database_sync_to_async
-    def save_message(self, message, user_id):
-        user = User.objects.get(id=user_id)
-        Notification.objects.create(user=user, message=message)
+    def save_message(self, message: str, user_id: int):
+        try:
+            user = User.objects.get(id=user_id)
+            Notification.objects.create(user=user, message=message)
+        except User.DoesNotExist:
+            # Handle the case where the user does not exist
+            pass
--- restaurant/routing.py
+++ restaurant/routing.py
@@ -1,6 +1,8 @@
-from . import consumers
 from django.urls import re_path
+from . import consumers
 
 websocket_urlpatterns = [
-    re_path(r'ws/socket-server/(?P<user_id>\w+)/$', consumers.ChatConsumer.as_asgi())
+    re_path(r'ws/customer/(?P<user_id>\w+)/$', consumers.ChatConsumer.as_asgi()),
+    re_path(r'ws/admin/(?P<user_id>\w+)/$', consumers.ChatConsumer.as_asgi()),
 ]
+
